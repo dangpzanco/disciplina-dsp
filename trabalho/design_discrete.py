663408b15734be68564e809436c3382181e193e8
Daniel Zanco <dangpzanco@gmail.com>
2019-06-06 17:52:06 -0300

    design discrete

--- trabalho/design_discrete.py
+++ trabalho/design_discrete.py
@@ -3,46 +3,36 @@ import matplotlib.pyplot as plt
 import scipy.signal as signal
 import scipy.linalg as lin
 import scipy.misc as misc
+import warnings
+warnings.filterwarnings('ignore', '.*Ill-conditioned matrix.*')
+warnings.filterwarnings('ignore', '.*Badly conditioned filter coefficients.*')
 
+def plot_zpk(system, fp, fs, Amax, Amin, sample_rate=48e3, num_samples=1024, ax=None):
+    fmin = np.floor(np.log10(fp))-1
+    fmax = np.ceil(np.log10(fs))
+    f = np.logspace(fmin, fmax, num_samples)
+    f, h = signal.freqz_zpk(*system, fs=sample_rate, worN=f)
+    print(fmin, fmax)
 
-def plot_zpk(z, p, k, fp, fs, Amax, Amin, sample_rate=48e3, num_samples=1024):
-    f = np.logspace(np.floor(np.log10(fp)), np.floor(np.log10(fp)), num_samples)
-    f, h = signal.freqz_zpk(z, p, k, fs=sample_rate, worN=num_samples)
+    if ax is None:
+        fig, ax = plt.subplots()
     
-    plt.plot(f, 20 * np.log10(abs(h)))
-    plt.xscale('log')
-    plt.title('Lowpass filter fit to constraints')
-    plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Amplitude [dB]')
-    plt.grid(True, which='both', axis='both')
-    plt.fill([1e2, 1e2,  fp,  fp], [-Amin*2, -Amax, -Amax, -Amin*2], '0.9', lw=0) # pass
-    plt.fill([fs, fs,  1e4,  1e4], [-Amin, Amax, Amax, -Amin], '0.9', lw=0) # stop
-    plt.axis([1e2, 1e4, -50, 1])
+    # Plot data
+    ax.plot(f, 20 * np.log10(np.abs(h)), linewidth=2)
+    ax.set_xscale('log')
 
-def plot_step(analog_system, discrete_system, num_samples=100, sample_rate=48e3, plot_error=False):
+    # Plot boxes
+    box_style = dict(linewidth=2, linestyle='--', edgecolor='k', facecolor='0.9')
+    ax.fill([10**fmin, 10**fmin,  fp,  fp], [-Amin*2, -Amax, -Amax, -Amin*2], **box_style) # pass
+    ax.fill([fs, fs,  10**fmax,  10**fmax], [-Amin, Amax, Amax, -Amin], **box_style) # stop
 
-    n = np.arange(num_samples)
-    t = n/sample_rate
+    # Set plot properties
+    ax.set_title('Lowpass filter fit to constraints')
+    ax.set_xlabel('Frequency [Hz]')
+    ax.set_ylabel('Amplitude [dB]')
+    ax.grid(True, which='both', axis='both')
+    ax.axis([1e2, 10**fmax, -50, 1])
 
-    _, ya = signal.step2(analog_system, T=t)
-    _, yd = signal.dstep((*discrete_system,1/sample_rate), t=t)
-
-
-    fig, ax1 = plt.subplots()
-    ax1.set_title('Invariância ao degrau')
-
-    ax1.plot(n,ya,'b')
-    ax1.plot(n,np.ravel(yd),'k.')
-    ax1.set_ylabel('Amplitude', color='b')
-    ax1.set_xlabel('Amostras')
-    ax1.grid(True)
-    ax1.legend(['Analógico', 'Discreto'])
-
-    if plot_error:
-        ax2 = ax1.twinx()
-        ax2.plot(n, np.abs(ya-np.ravel(yd)), 'r')
-        ax2.set_ylabel('Erro', color='r')
-        ax2.axis('tight')
 
 def plot_time(analog_system, discrete_system, response='step', num_samples=100, sample_rate=48e3, plot_error=False):
 
@@ -91,26 +81,77 @@ def matched_method(z, p, k, dt):
 
     return zd, pd, kd, dt
 
-def get_filter(fp, fs, Amax=1, Amin=42, sample_rate=48e3, filter_type='but', method='zoh'):
+def get_filter(spec, filter_type='but', method='zoh'):
     
     if filter_type.lower() in ('butterworth'):
-        N, Wn = signal.buttord(2*np.pi*fp, 2*np.pi*fs, Amax, Amin, analog=True)
+        N, Wn = signal.buttord(2*np.pi*spec['fp'], 2*np.pi*spec['fs'], spec['Amax'], spec['Amin'], analog=True)
         z, p, k = signal.butter(N, Wn, output='zpk', btype='low', analog=True)
     elif filter_type.lower() in ('cauer' + 'elliptic'):    
-        N, Wn = signal.ellipord(2*np.pi*fp, 2*np.pi*fs, Amax, Amin, analog=True)
-        z, p, k = signal.ellip(N, Amax, Amin, Wn, output='zpk', btype='low', analog=True)
+        N, Wn = signal.ellipord(2*np.pi*fp, 2*np.pi*spec['fs'], spec['Amax'], spec['Amin'], analog=True)
+        z, p, k = signal.ellip(N, spec['Amax'], spec['Amin'], Wn, output='zpk', btype='low', analog=True)
 
     if method == 'matched':
-        zd, pd, kd, dt = matched_method(z, p, k, 1/sample_rate)
+        zd, pd, kd, dt = matched_method(z, p, k, spec['dt'])
     else:
-        zd, pd, kd, dt = signal.cont2discrete((z,p,k), 1/sample_rate, method=method)
+        zd, pd, kd, dt = signal.cont2discrete((z,p,k), spec['dt'], method=method)
 
     analog_system = (z,p,k)
     discrete_system = (zd,pd,kd)
 
     return analog_system, discrete_system
 
+def check_limits(system, spec, num_samples=1000):
+    # fp, fs, Amax=1, Amin=42, sample_rate=48e3
 
+    f1 = np.logspace(np.floor(np.log10(spec['fp']))-1, np.log10(spec['fp']), 2*num_samples)
+    f2 = np.logspace(np.log10(spec['fs']), np.log10(spec['sample_rate']/2), num_samples)
+    # f1 = np.linspace(0, spec['fp'], 2*num_samples)
+    # f2 = np.linspace(spec['fs'], spec['sample_rate']/2, num_samples)
+    f = np.hstack([f1, f2])
+
+    f, h = signal.freqz_zpk(*system, fs=sample_rate, worN=f)
+    Hdb = 20 * np.log10(np.abs(h))
+
+    pass_band = Hdb[f <= spec['fp']]
+    pass_band_faults = (pass_band < -spec['Amax']).sum() + (pass_band > 0).sum()
+    stop_band_faults = (Hdb[f >= spec['fs']] > -spec['Amin']).sum()
+
+    total_faults = pass_band_faults + stop_band_faults + system[1].size
+
+    return total_faults
+
+
+def optimize_filter(spec, min_order=4, filter_type='but', method='zoh', num_samples=100, limits_samples=1000):
+    # fp, fs, Amax, Amin, sample_rate
+    original_spec = spec.copy()
+    # Amax_vec = np.linspace(spec['Amax'], 1e-3, num_samples)
+    # Amin_vec = np.linspace(spec['Amin'], 2*spec['Amin'], num_samples)
+    Amax_vec = np.random.uniform(1e-3, spec['Amax'], num_samples)
+    Amin_vec = np.random.uniform(spec['Amin'], 2*spec['Amin'], num_samples)
+    Avec = np.vstack([Amax_vec, Amin_vec])
+
+    faults = np.inf
+    for i in range(num_samples):
+        spec['Amax'] = Avec[0,i]
+        spec['Amin'] = Avec[1,i]
+        asys, dsys = get_filter(spec, filter_type=filter_type, method=method)
+        total_faults = check_limits(dsys, original_spec, num_samples=limits_samples)
+        total_faults -= min_order
+
+        filter_order = dsys[1].size
+        print(total_faults, filter_order)
+
+        if faults > total_faults:
+            faults = total_faults
+            analog_system, discrete_system = (asys, dsys)
+        
+        if faults == 0:
+            analog_system, discrete_system = (asys, dsys)
+            return analog_system, discrete_system, spec
+    
+    print(faults, discrete_system[1].size)
+
+    return analog_system, discrete_system, spec
 
 
 
@@ -122,38 +163,18 @@ fs = 3.5e3
 Amax = 1
 Amin = 42
 
-
-N, Wn = signal.buttord(2*np.pi*fp, 2*np.pi*fs, Amax, Amin, analog=True)
-
-print(f'Amax = {Amax} dB, Amin = {Amin} dB')
-print(f'fp = {fp/1e3} kHz, fs = {fs/1e3} kHz')
-print(f'Butterworth order = {N}')
-print(f'Wn = 2π * {Wn/2/np.pi:.3f}Hz')
-
-z, p, k = signal.butter(N, Wn, output='zpk', btype='low', analog=True)
-b, a = signal.zpk2tf(z, p, k)
-
-print(f'z = {z}')
-print(f'p = {p}')
-print(f'k = {k}')
-print(f'b = {b}')
-print(f'a = {a}')
+spec = dict(fp=fp, fs=fs, Amax=Amax, Amin=Amin, sample_rate=sample_rate, dt=1/sample_rate)
 
 
 
-zd, pd, kd, dt = signal.cont2discrete((z,p,k), 1/sample_rate, method='zoh', alpha=None)
-
-print(f'zd = {zd}')
-print(f'pd = {pd}')
-print(f'kd = {kd}')
 
 
-# analog_system = (z,p,k)
-# discrete_system = (zd,pd,kd)
-analog_system, discrete_system = get_filter(fp, fs, Amax=1, Amin=42, sample_rate=48e3, filter_type='but', method='foh')
-plot_zpk(*discrete_system, fp, fs, Amax, Amin)
+analog_system, discrete_system, final_spec = optimize_filter(spec, filter_type='but', method='zoh', num_samples=1000)
+print(final_spec)
 
-plot_time(analog_system, discrete_system, response='ramp', num_samples=100, sample_rate=sample_rate, plot_error=False)
+plot_zpk(discrete_system, fp, fs, Amax, Amin)
+
+# plot_time(analog_system, discrete_system, response='step', num_samples=100, sample_rate=sample_rate, plot_error=False)
 plt.show()
 
 
